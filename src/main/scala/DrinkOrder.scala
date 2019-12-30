5eb47a2a68167613ea40497cb69bf0190990279b
Emiliano Menéndez <emystein@gmail.com>
2019-12-30 09:52:18 -0300

    Remove unused methods

--- src/main/scala/DrinkOrder.scala
+++ src/main/scala/DrinkOrder.scala
@@ -1,3 +1,1 @@
-case class DrinkOrder(flavor: String, sugarCount: Int = 0, temperature: Temperature = NormalTemperature()) {
-  def includeStick: Boolean = sugarCount > 0
-}
+case class DrinkOrder(flavor: String, sugarCount: Int = 0, temperature: Temperature = NormalTemperature())
--- src/main/scala/SalesStats.scala
+++ src/main/scala/SalesStats.scala
@@ -3,8 +3,6 @@ import scala.collection.mutable
 class SalesStats {
   var salesSumByFlavor: mutable.Map[String, DrinkStat] = mutable.HashMap()
 
-  def clear = salesSumByFlavor.clear
-
   def add(drink: Drink, price: Double) = {
     val statItem = salesSumByFlavor.getOrElse(drink.flavor, new DrinkStat(drink.flavor, 0, 0))
     statItem.salesCount += 1
